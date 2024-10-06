453d6c574b512b623ead3dd6f6a0d7d8963dc0d7
tusharupase258 <tusharupase786@gmail.com>
2024-10-06 13:36:44 +0530

    updated

---
+++ Sample_code_Terraform/01.Modules/variables.tf
@@ -0,0 +1,31 @@
+variable "tusharRGvarsm" {
+  type = map(any)
+}
+
+variable "tusharvnetvarsm" {
+  type = map(any)
+}
+
+variable "tusharsubnetmvars" {
+  type = map(any)
+}
+
+variable "tusharpublicipsvarm" {
+  type = map(any)
+}
+
+variable "tusharnicsvarsm" {
+  type = map(any)
+}
+
+variable "tusharkeyvaultsvarsm" {
+  type = map(any)
+}
+
+variable "tusharpasswdsvarsm" {
+  type = map(any)
+}
+
+variable "tusharvmsvarsm" {
+  type = map(any)
+}
